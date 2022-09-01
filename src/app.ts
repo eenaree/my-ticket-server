22505458d8adce74cb068779b8fd777e743f4e5a
eenaree <narilee91@gmail.com>
2022-09-01 09:05:08 +0900

    Initial commit
    - 프로젝트 기본 세팅

---
+++ src/app.ts
@@ -0,0 +1,52 @@
+import * as cors from 'cors';
+import * as dotenv from 'dotenv';
+import * as express from 'express';
+import * as session from 'express-session';
+import * as morgan from 'morgan';
+import routes from '@routes';
+
+dotenv.config();
+
+const app = express();
+
+const prod = process.env.NODE_ENV === 'production';
+
+app.set('port', process.env.PORT || 8080);
+
+app.use(
+  cors({
+    origin: 'http://localhost:4000',
+    credentials: true,
+  })
+);
+app.use(morgan(prod ? 'combined' : 'dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET as string,
+    cookie: {
+      secure: prod || false,
+    },
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.use('/api', routes);
+
+const errorHandler: express.ErrorRequestHandler = (err, req, res, next) => {
+  if (err instanceof Error) {
+    if (res.headersSent) {
+      return next(err);
+    }
+    console.error(err.stack);
+    res.status(500).send({ message: '서버 오류가 발생했습니다.' });
+  }
+};
+
+app.use(errorHandler);
+
+app.listen(app.get('port'), () => {
+  console.log(`${app.get('port')} 번 포트에서 서버 대기 중...`);
+});
