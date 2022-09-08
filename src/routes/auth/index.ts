eb3f4325be3a891b781b54916930bd521f5d7f61
Nari Lee <narilee91@gmail.com>
2022-09-08 11:31:53 +0900

    Feat: login, logout 라우터 추가 (#15)
    * Feat: strategy redirect 경로 변경
    
    * Feat: login, logout 라우터 추가

--- src/routes/auth/index.ts
+++ src/routes/auth/index.ts
@@ -8,7 +8,7 @@ router.get('/kakao', passport.authenticate('kakao'));
 router.get(
   '/kakao/callback',
   passport.authenticate('kakao', {
-    successRedirect: CLIENT_URL,
+    successRedirect: `${CLIENT_URL}/authenticated`,
   })
 );
 
@@ -16,7 +16,7 @@ router.get('/google', passport.authenticate('google'));
 router.get(
   '/google/callback',
   passport.authenticate('google', {
-    successRedirect: CLIENT_URL,
+    successRedirect: `${CLIENT_URL}/authenticated`,
   })
 );
 
@@ -24,8 +24,29 @@ router.get('/naver', passport.authenticate('naver'));
 router.get(
   '/naver/callback',
   passport.authenticate('naver', {
-    successRedirect: CLIENT_URL,
+    successRedirect: `${CLIENT_URL}/authenticated`,
   })
 );
+
+router.get('/login', (req, res) => {
+  if (req.user) {
+    res.json({
+      success: true,
+      message: '로그인 성공',
+      user: req.user,
+    });
+  } else {
+    res.json({ success: false, message: '로그인 실패' });
+  }
+});
+
+router.post('/logout', (req, res, next) => {
+  req.logout(error => {
+    if (error) {
+      return next(error);
+    }
+    res.json({ success: true, message: '로그아웃 성공' });
+  });
+});
 
 export default router;
