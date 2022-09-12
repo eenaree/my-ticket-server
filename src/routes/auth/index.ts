49fd014f60e3115af33c27a381a3bd1fef5eafc5
Nari Lee <narilee91@gmail.com>
2022-09-12 16:46:59 +0900

    Refactor: auth router handler, controller로 분리 (#17)
    router를 처리하는 코드 로직을 controller 파일로 별도 분리하여 변수에 저장하여 export함.
    지금은 간단한 로직 밖에 없어서 큰 의미는 없다고 볼 수 있지만,
    추후 미들웨어 등이 존재하여 처리해야 하는 로직이 많아질 때,
    어떤 작업을 실행하는지 쉽게 알 수 있도록 하기 위해 변경함.

--- src/routes/auth/index.ts
+++ src/routes/auth/index.ts
@@ -1,52 +1,18 @@
 import * as express from 'express';
-import * as passport from 'passport';
+import * as controller from './auth.controller';
 
 const router = express.Router();
-const CLIENT_URL = 'http://localhost:4000';
 
-router.get('/kakao', passport.authenticate('kakao'));
-router.get(
-  '/kakao/callback',
-  passport.authenticate('kakao', {
-    successRedirect: `${CLIENT_URL}/authenticated`,
-  })
-);
+router.get('/kakao', controller.kakaoAuth);
+router.get('/kakao/callback', controller.kakaoAuthCallback);
 
-router.get('/google', passport.authenticate('google'));
-router.get(
-  '/google/callback',
-  passport.authenticate('google', {
-    successRedirect: `${CLIENT_URL}/authenticated`,
-  })
-);
+router.get('/google', controller.googleAuth);
+router.get('/google/callback', controller.googleAuthCallback);
 
-router.get('/naver', passport.authenticate('naver'));
-router.get(
-  '/naver/callback',
-  passport.authenticate('naver', {
-    successRedirect: `${CLIENT_URL}/authenticated`,
-  })
-);
+router.get('/naver', controller.naverAuth);
+router.get('/naver/callback', controller.naverAuthCallback);
 
-router.get('/login', (req, res) => {
-  if (req.user) {
-    res.json({
-      success: true,
-      message: '로그인 성공',
-      user: req.user,
-    });
-  } else {
-    res.status(401).json({ success: false, message: '로그인 실패' });
-  }
-});
-
-router.post('/logout', (req, res, next) => {
-  req.logout(error => {
-    if (error) {
-      return next(error);
-    }
-    res.json({ success: true, message: '로그아웃 성공' });
-  });
-});
+router.get('/login', controller.login);
+router.post('/logout', controller.logout);
 
 export default router;
