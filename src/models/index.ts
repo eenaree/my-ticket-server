3780919151a71c912864a7d9e16020fb898aa143
Nari Lee <narilee91@gmail.com>
2022-09-23 22:13:21 +0900

    Feat: my teams 리스트 요청 및 변경에 팀 선호도 값 제어 로직 추가 (#31)
    - 별도 모델 생성없이, through table에 preference column 추가
    
    - 팀 변경시, 새 팀과 새 팀에 남아있는 기존 팀의 로직을 같은 단위에서 처리하고 싶었으나,
    기존 팀의 선호도 값을 갖고 올 수 있는 방법을 생각해낼 수 없어
    존재하지 않는 기존 팀, 남아있는 기존 팀, 새로운 팀 → 총 3가지로 나눠, 병렬 처리함.
    
    - 팀 리스트 요청시, 선호도 값 순서대로 정렬하여 응답 처리.

--- src/models/index.ts
+++ src/models/index.ts
@@ -2,6 +2,7 @@ import { Sequelize } from 'sequelize';
 import { dbConfig } from '@config';
 import { ENV } from '@typings/db';
 import Team from './team';
+import Team_Fans from './team_fans';
 import User from './user';
 
 const env = (process.env.NODE_ENV as ENV) || 'development';
@@ -16,8 +17,9 @@ const sequelize = new Sequelize(
 
 User.initialize(sequelize);
 Team.initialize(sequelize);
+Team_Fans.initialize(sequelize);
 
-User.belongsToMany(Team, { through: 'TeamFans' });
-Team.belongsToMany(User, { through: 'TeamFans', as: 'Fans' });
+User.belongsToMany(Team, { through: 'Team_Fans' });
+Team.belongsToMany(User, { through: 'Team_Fans', as: 'Fans' });
 
 export { sequelize };
