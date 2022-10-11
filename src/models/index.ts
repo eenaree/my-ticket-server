eaf013deec5f0996225b9c0e70ad171b9c183cdd
Nari Lee <narilee91@gmail.com>
2022-10-12 02:21:44 +0900

    Feat(association): Ticket, Season, Series, Season model db 연결 및 관계 추가

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,8 +1,12 @@
 import { Sequelize } from 'sequelize';
 import { dbConfig } from '@config';
 import { ENV } from '@typings/db';
+import Season from './season';
+import Series from './series';
+import Stadium from './stadium';
 import Team from './team';
 import Team_Fans from './team_fans';
+import Ticket from './ticket';
 import User from './user';
 
 const env = (process.env.NODE_ENV as ENV) || 'development';
@@ -18,8 +22,30 @@ const sequelize = new Sequelize(
 User.initialize(sequelize);
 Team.initialize(sequelize);
 Team_Fans.initialize(sequelize);
+Season.initialize(sequelize);
+Series.initialize(sequelize);
+Stadium.initialize(sequelize);
+Ticket.initialize(sequelize);
 
 User.belongsToMany(Team, { through: 'Team_Fans' });
 Team.belongsToMany(User, { through: 'Team_Fans', as: 'Fans' });
+
+Season.hasMany(Series);
+Series.belongsTo(Season);
+
+Team.hasMany(Stadium);
+Stadium.belongsTo(Team);
+
+User.hasMany(Ticket);
+Ticket.belongsTo(User);
+
+Season.hasMany(Ticket);
+Ticket.belongsTo(Season);
+
+Series.hasMany(Ticket);
+Ticket.belongsTo(Series);
+
+Stadium.hasMany(Ticket);
+Ticket.belongsTo(Stadium);
 
 export { sequelize };
