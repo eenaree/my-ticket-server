4f7677b4841bf65b4e7506bbf953a60ddc5f48bf
Nari Lee <narilee91@gmail.com>
2022-10-22 18:08:37 +0900

    Feat: implement getMyTickets route pagination

--- src/typings/db.ts
+++ src/typings/db.ts
@@ -1,1 +1,8 @@
+import * as express from 'express';
+import { ParsedQs } from 'qs';
+
 export type ENV = 'development' | 'test' | 'production';
+
+export interface TypedExpressQuery<T extends ParsedQs> extends express.Request {
+  query: T;
+}
