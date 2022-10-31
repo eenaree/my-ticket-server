ae5596a014cb8d4d3bfd9a02ddf8acc2c4b98c4c
Nari Lee <narilee91@gmail.com>
2022-11-01 00:12:50 +0900

    Refactor: auth routes error handling

--- src/middlewares/isLoggedIn.ts
+++ src/middlewares/isLoggedIn.ts
@@ -1,9 +1,10 @@
 import * as express from 'express';
+import { UnauthorizedError } from '~/lib/AppError';
 
 export const isLoggedIn: express.RequestHandler = (req, res, next) => {
   if (req.isAuthenticated()) {
     next();
   } else {
-    res.status(401).json({ success: false, message: '로그인이 필요합니다.' });
+    throw new UnauthorizedError();
   }
 };
